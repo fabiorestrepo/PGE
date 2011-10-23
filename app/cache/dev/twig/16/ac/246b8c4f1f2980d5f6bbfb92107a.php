3111838ab046afbe86f8af1b20e03a78b320e8db
Fabio Restrepo <fabiorestrepo1245@gmail.com>
2011-10-23 18:19:58 -0500

    Se Instala los vendors

---
+++ app/cache/dev/twig/16/ac/246b8c4f1f2980d5f6bbfb92107a.php
@@ -0,0 +1,52 @@
+<?php
+
+/* WebProfilerBundle:Profiler:toolbar_item.html.twig */
+class __TwigTemplate_16ac246b8c4f1f2980d5f6bbfb92107a extends Twig_Template
+{
+    protected function doGetParent(array $context)
+    {
+        return false;
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $context = array_merge($this->env->getGlobals(), $context);
+
+        // line 1
+        if ($this->getContext($context, "link")) {
+            // line 2
+            echo "    ";
+            ob_start();
+            // line 3
+            echo "        <a style=\"text-decoration: none; margin: 0; padding: 0;\" href=\"";
+            echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_profiler", array("token" => $this->getContext($context, "token"), "panel" => $this->getContext($context, "name"))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, $this->getContext($context, "icon"), "html", null, true);
+            echo "</a>
+    ";
+            $context["icon"] = new Twig_Markup(ob_get_clean());
+        }
+        // line 6
+        echo "<span style=\"white-space:nowrap; color:#2f2f2f; display:inline-block; min-height:24px; border-right:1px solid #cdcdcd; padding:5px 7px 5px 4px; \">
+     ";
+        // line 7
+        echo twig_escape_filter($this->env, ((array_key_exists("icon", $context)) ? (twig_default_filter($this->getContext($context, "icon"), "")) : ("")), "html", null, true);
+        echo "
+     ";
+        // line 8
+        echo twig_escape_filter($this->env, ((array_key_exists("text", $context)) ? (twig_default_filter($this->getContext($context, "text"), "")) : ("")), "html", null, true);
+        echo "
+</span>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "WebProfilerBundle:Profiler:toolbar_item.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
---
+++ app/cache/dev/twig/9c/27/304e4b87962316bcb9a21be77ec4.php
@@ -0,0 +1,62 @@
+<?php
+
+/* WebProfilerBundle:Profiler:toolbar_js.html.twig */
+class __TwigTemplate_9c27304e4b87962316bcb9a21be77ec4 extends Twig_Template
+{
+    protected function doGetParent(array $context)
+    {
+        return false;
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $context = array_merge($this->env->getGlobals(), $context);
+
+        // line 1
+        echo "<div id=\"sfwdt";
+        echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+        echo "\" style=\"display: none\"></div>
+<script type=\"text/javascript\">/*<![CDATA[*/
+    (function () {
+        var wdt, xhr;
+        wdt = document.getElementById('sfwdt";
+        // line 5
+        echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+        echo "');
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else {
+            xhr = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+        xhr.open('GET', '";
+        // line 11
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_wdt", array("token" => $this->getContext($context, "token"))), "html", null, true);
+        echo "', true);
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.onreadystatechange = function(state) {
+            if (4 === xhr.readyState && 200 === xhr.status && -1 !== xhr.responseText.indexOf('sf-toolbarreset')) {
+                wdt.innerHTML = xhr.responseText;
+                wdt.style.display = 'block';
+            } else if (4 === xhr.readyState && xhr.status != 200) {
+                confirm('An error occurred while loading the web debug toolbar (' + xhr.status + ': ' + xhr.statusText + ').\\n\\nDo you want to open the profiler?') && (window.location = '";
+        // line 18
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_profiler", array("token" => $this->getContext($context, "token"))), "html", null, true);
+        echo "');
+            }
+        };
+        xhr.send('');
+    })();
+/*]]>*/</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "WebProfilerBundle:Profiler:toolbar_js.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
